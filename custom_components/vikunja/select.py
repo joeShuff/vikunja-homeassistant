8feacc10a41de5d2fa1abbd23e7832aafc5c0a30
Joe Shufflebotham <joe.shufflebotham13@gmail.com>
2025-02-13 18:25:08 +0000

    add repeat mode config options

---
+++ custom_components/vikunja/select.py
@@ -0,0 +1,40 @@
+from pyvikunja.api import VikunjaAPI
+
+from custom_components.vikunja import LOGGER
+from custom_components.vikunja.const import DATA_TASKS_KEY
+from custom_components.vikunja.sensors.task.repeat_mode_sensors import *
+
+
+def get_select_for_task(coordinator, base_url, task_id):
+    return [
+        VikunjaRepeatModeSelect(coordinator, base_url, task_id),
+        VikunjaRepeatIntervalUnitSensor(coordinator, base_url, task_id)
+    ]
+
+
+async def async_setup_entry(hass, entry, async_add_entities):
+    LOGGER.info("Setting up Vikunja selects...")
+
+    # Get stored API instance and fetched data
+    vikunja_data = hass.data.get("vikunja", {}).get(entry.entry_id)
+    if not vikunja_data:
+        LOGGER.error("No Vikunja data found in hass.data")
+        return False
+
+    vikunja_api: VikunjaAPI = vikunja_data["api"]
+    coordinator = vikunja_data["coordinator"]
+
+    # Create sensor entities
+    entities = []
+
+    tasks = coordinator.data[DATA_TASKS_KEY].keys()
+
+    for task_id in tasks:
+        LOGGER.info(f"Task is {task_id}")
+        entities.extend(get_select_for_task(coordinator, vikunja_api.web_ui_link, task_id))
+
+    if not entities:
+        LOGGER.warning("No entities created")
+
+    async_add_entities(entities, True)
+    LOGGER.info(f"Added {len(entities)} Vikunja selects.")
