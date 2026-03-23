5ef0508a6cc4c84ee400219c30243f656fbab9f0
Deosrc <deosrc@g-de.co.uk>
2026-03-23 00:31:09 +0000

    Add entity setup filters when task devices are off

--- custom_components/vikunja/datetime.py
+++ custom_components/vikunja/datetime.py
@@ -1,5 +1,8 @@
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
 from pyvikunja.api import VikunjaAPI
 
+from custom_components.vikunja.const import CONF_TASKS_AS_DEVICES
 from custom_components.vikunja.sensors.TaskSensors import *
 
 
@@ -10,7 +13,7 @@ def get_datetime_sensors_for_task(coordinator, base_url, task_id):
     ]
 
 
-async def async_setup_entry(hass, entry, async_add_entities):
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_entities):
     LOGGER.info("Setting up Vikunja sensors...")
 
     # Get stored API instance and fetched data
@@ -27,9 +30,10 @@ async def async_setup_entry(hass, entry, async_add_entities):
 
     tasks = coordinator.data[DATA_TASKS_KEY].keys()
 
-    for task_id in tasks:
-        LOGGER.info(f"Task is {task_id}")
-        entities.extend(get_datetime_sensors_for_task(coordinator, vikunja_api.web_ui_link, task_id))
+    if entry.data.get(CONF_TASKS_AS_DEVICES, True):
+        for task_id in tasks:
+            LOGGER.info(f"Task is {task_id}")
+            entities.extend(get_datetime_sensors_for_task(coordinator, vikunja_api.web_ui_link, task_id))
 
     if not entities:
         LOGGER.warning("No entities created")
